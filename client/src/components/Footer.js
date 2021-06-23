c48476c77323de62795ccc295a6b1378fe0336f8
Miguel Martinez <miguel.martinez11@gmail.com>
2021-06-23 20:03:34 +0200

    work on footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Footer=()=>{
   return (
     <div className="footer">
-      {/* <div style={{textAlign: 'center',margin: '1em 0'}}>
+      <div style={{textAlign: 'center',margin: '1em 0'}}>
       &copy; Miguel Martinez
-      </div> */}
+      </div>
     </div>
     
   )
