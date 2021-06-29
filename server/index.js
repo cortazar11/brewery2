dd678a915424ba9827f37b5291ea802f8de1f98f
Miguel Martinez <miguel.martinez11@gmail.com>
2021-06-29 10:16:29 +0200

    initial commit in dev

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ mongoose.connect(keys.mongoURI, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 },()=>{
-  console.log('mongoose connected')
+  console.log('mongoose connecteddddd')
 });
 
 authRoutes(app);
